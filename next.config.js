8ed06420855e26e384d8dc65d663d025be56d0d0
nobitaany <nobitaany@gmail.com>
2026-07-11 00:21:47 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingIncludes: {
-    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
-  },
-};
-
-module.exports = nextConfig;
